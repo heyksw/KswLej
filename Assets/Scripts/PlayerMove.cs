9106489b51e682fa880fe5b82d43a02ea4434b62
heyksw <heyksw0208@naver.com>
2021-01-25 16:06:33 +0900

    첫 테스트
    커밋 처음

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float jumpPower;
+    bool isJump;
+    Rigidbody2D rigid;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump") && !isJump)
+        {
+            isJump = true;
+            rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+
+        rigid.AddForce(Vector2.right * h*0.2f, ForceMode2D.Impulse);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        //if (collision.gameObject.name == "Floor (2)")
+            isJump = false;
+    }
+}
